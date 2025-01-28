83eae98910fae569191e1d2c518bf1065a70f91f
studioclips-mastermonk <studioclips@gmail.com>
2025-01-28 13:47:30 +0900

    問題7

--- blog.php
+++ blog.php
@@ -7,25 +7,9 @@ if( !isset($_SESSION['userKey']) ){
     exit();
 }
 
-
+//記事の表示
 if( isset($_GET['key']) ){
-    $dbh = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
-    $sql = '
-    SELECT P.id , p.title , p.body , U.userName , U.userKey
-    FROM `category` as C
-    JOIN `posts` as P
-    ON C.id = P.category_id
-    JOIN `users` as U
-    ON P.user_key = U.userKey
-    WHERE
-    C.category_key = :category_key;
-    ';
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(':category_key', $_GET['key']);
-    //クエリの実行
-    $stmt->execute();
-    //実行結果を取得
-    $data = $stmt->fetchAll();
+
 }else{
     header("Location: {$const(DIR)}"); //TOPに遷移
     exit();
@@ -37,36 +21,7 @@ if($_POST){
     $body = $_POST['body'];
     if($title != '' && $body !=''){
         try{
-            $dbh = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
-            $sql= 'SELECT `id` FROM `category` WHERE `category_key` = :category_key';
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindValue(':category_key', $_GET['key']);
-            //クエリの実行
-            $stmt->execute();
-            $category_id = $stmt->fetch();
-
-
-            $sql = '
-            INSERT INTO `posts`
-            (`title`, `body`, `user_key`, `category_id`)
-            VALUES
-            (:title,:body,:user_key,:category_id)';
-
-
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindValue(':title', $title);
-            $stmt->bindValue(':body', $body);
-            $stmt->bindValue(':user_key', $_SESSION['userKey']);
-            $stmt->bindValue(':category_id', $category_id["id"]);
-            //クエリの実行
-            $result = $stmt->execute();
-            if( $result ){
-                //登録したら現在のURLに遷移
-                header("Location: {$_SERVER['REQUEST_URI']}");
-                exit();
-            }else{
-                $error[] = '登録できませんでした。';
-            }
+            
         } catch (Exception $e) {
             var_dump( $e->getMessage() );
         }
